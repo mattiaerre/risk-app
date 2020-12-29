0322099acff8bb75deceef1603b7bd3e3c8947f2
mattia richetto <mattia.richetto@gmail.com>
2020-12-28 22:55:30 -0800

    add ui for battle

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { battle } from 'risk-core';
 import './App.css';
 
+function makeDescription(result) {
+  if (result === null) {
+    return '';
+  }
+  let description = '';
+  if (result.defender.lost !== 0) {
+    description += `Defender loses ${result.defender.lost} ${
+      result.defender.lost === 1 ? 'troop' : 'troops'
+    }`;
+  }
+  if (result.attacker.lost !== 0) {
+    if (description.length > 0) {
+      description += ', and ';
+    }
+    description += `Attacker loses ${result.attacker.lost} ${
+      result.attacker.lost === 1 ? 'troop' : 'troops'
+    }`;
+  }
+  return description;
+}
+
 function App() {
+  const [attackerDice, setAttackerDice] = useState(3);
+  const [defenderDice, setDefenderDice] = useState(2);
+  const [result, setResult] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App__main">
+      <label>
+        <select onChange={(e) => setAttackerDice(parseInt(e.target.value, 10))}>
+          <option value={3}>3</option>
+          <option value={2}>2</option>
+          <option value={1}>1</option>
+        </select>
+        attacker
+      </label>
+      <label>
+        <select onChange={(e) => setDefenderDice(parseInt(e.target.value, 10))}>
+          <option value={2}>2</option>
+          <option value={1}>1</option>
+        </select>
+        defender
+      </label>
+      <button
+        onClick={() => {
+          setResult(battle({ attacker: attackerDice, defender: defenderDice }));
+        }}
+      >
+        3, 2, 1 ...
+      </button>
+      <button onClick={() => setResult(null)}>Clear</button>
+      <code>{JSON.stringify(result, null, 2)}</code>
+      <p>{makeDescription(result)}</p>
+    </main>
   );
 }
 
